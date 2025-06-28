39e7bd9f04e3ed2a5943b3838aed1eaccf5021a9
SmileYik <miskyle@outlook.com>
2025-06-28 08:56:04 +0800

    clean

--- src/main/java/org/eu/smileyik/luaInMinecraftBukkitII/JsonUtil.java
+++
@@ -1,7 +0,0 @@
-package org.eu.smileyik.luaInMinecraftBukkitII;
-
-public class JsonUtil {
-    public static String stripComments(String input) {
-        return input.replaceAll("[ \\t]+//.+\\n", "");
-    }
-}
