871ffd4f191b59481239d6e8d6eeed2c070fb1d2
SmileYik <miskyle@outlook.com>
2025-06-30 20:52:12 +0800

    ILuaCommandClassBuilder: update docs

--- src/main/java/org/eu/smileyik/luaInMinecraftBukkitII/api/lua/luaState/command/ILuaCommandClassBuilder.java
+++ src/main/java/org/eu/smileyik/luaInMinecraftBukkitII/api/lua/luaState/command/ILuaCommandClassBuilder.java
@@ -5,6 +5,66 @@ import org.eu.smileyik.luajava.type.ILuaCallable;
 import org.eu.smileyik.luajava.type.LuaTable;
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * 指令类建造器. 指令类是一个包含多条指令的集合, 并且指令类也有自己的名称.
+ * <p>
+ *     举个例子, 现有如下指令:
+ *     <pre><code>
+ *         /item get [name]
+ *         /item store [name]
+ *         /item nbt read [key]
+ *         /item nbt write [key] [value]
+ *     </code></pre>
+ * </p>
+ * <p>
+ *     既然指令类是一个包含多条指令的集合, 而每一条实际指令方法之间又没有层级关系,
+ *     只能匹配指令的固定词还有它的参数, 那么, 指令之间的层级关系就由指令类之间的层级关系生成.
+ *     以上的四条指令可以抽取成两个指令类, 分别如下:
+ *     <li>
+ *         item 指令类: 包含指令 <code>get [name]</code>, <code>store [name]</code>.
+ *     </li>
+ *     <li>
+ *         nbt 指令类: 包含指令 <code>read [key]</code>, <code>write [key] [value]</code>
+ *     </li>
+ * </p>
+ * <p>
+ *     为了让<code>item</code>指令类与<code>nbt</code>指令类之间有层级关系,我们可以在构建时,
+ *     先构造<code>item</code>指令类, 直接使用<code>build(String)</code>方法,
+ *     再构造<code>nbt</code>指令类, 使用<code>build(String, String)</code>方法.
+ *     之后注册指令时, 就可以将两个指令类型一起注册. 而在 Lua 中的实际注册代码可以参考以下代码:
+ *     <pre><code>
+ *          -- 构造 item 指令类
+ *          local itemCommandClass = luaBukkit.env:commandClassBuilder()
+ *             :command("get")      -- get 指令
+ *                 :args({"name"})
+ *                 :description("get a item")
+ *                 :handler(function(sender, args) doSomething() end)
+ *             :command("store")    -- store 指令
+ *                 :args({"name"})
+ *                 :description("store a item and named it")
+ *                 :handler(function(sender, args) doSomething() end)
+ *             :build("item")
+ *
+ *          -- 构造 nbt 指令类
+ *          local nbtCommandClass = luaBukkit.env:commandClassBuilder()
+ *             :command("read")     -- read 指令
+ *                 :args({"key"})
+ *                 :description("read item's nbt key")
+ *                 :handler(function(sender, args) doSomething() end)
+ *             :command("write")    -- write 指令
+ *                 :args({"key", "value"})
+ *                 :description("write key-value to item' nbt")
+ *                 :handler(function(sender, args) doSomething() end)
+ *             :build("nbt", "item") -- 设定 nbt 类的父指令类为 item 类
+ *
+ *          -- 注册指令, 并且将最顶层, 也就是没有父指令类的指令类名称写到第一个形参中,
+ *          -- 并且将两个指令类组成数组风格的Table, 传入第二个形参.
+ *          local result = luaBukkit.env:registerCommand("item", {itemCommandClass, nbtCommandClass})
+ *          if result:isError() then luaBukkit.log:info("Register command failed!") end
+ *     </code></pre>
+ * </p>
+ *
+ */
 public interface ILuaCommandClassBuilder {
     /**
      * 设定指令别名, 当且仅当该指令为顶级指令时有效
@@ -168,7 +228,31 @@ public interface ILuaCommandClassBuilder {
      */
     Class<?> build(LuaTable metaTable, LuaTable... commandTables) throws Exception;
 
+    /**
+     * 构建指令
+     * @param command 根指令名
+     * @return 构建好的指令类
+     */
     Class<?> build(@NotNull String command);
 
+    /**
+     * 构建指令, 并将次类型归类在指定父级指令下. 例如有两个指令类,
+     * <li>
+     *     指令类1的顶级指令名为 <code>item</code>, 其中包含子指令: <code>get</code>, <code>set</code>
+     * </li>
+     * <li>
+     *     指令类2的顶级指令名为 <code>nbt</code>, 其中包含子指令: <code>read</code>, <code>clear</code>.
+     * </li>
+     * 此时构建指令时, 将指令2的父级指令名设置为指令1, 注册完指令后, 实际生成的指令是这样的:
+     * <pre><code>
+     *      /item get
+     *      /item set
+     *      /item nbt read
+     *      /item nbt clear
+     * </code></pre>
+     * @param command       顶级指令名
+     * @param parentCommand 父级指令名
+     * @return 构建好的指令类
+     */
     Class<?> build(@NotNull String command, String parentCommand);
 }
