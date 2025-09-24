754b3e9858548fd0ae4c1c01197b8de671422ea7
SmileYik <miskyle@outlook.com>
2025-09-24 08:39:25 +0800

    chore: strip main method to LibraryHashes class

---
+++ src/main/java/org/eu/smileyik/luaInMinecraftBukkitII/util/HashUtil.java
@@ -0,0 +1,25 @@
+package org.eu.smileyik.luaInMinecraftBukkitII.util;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class HashUtil {
+    public static String sha256(File file) throws NoSuchAlgorithmException, IOException {
+        byte[] bytes = Files.readAllBytes(file.toPath());
+        return sha256(bytes);
+    }
+
+    public static String sha256(byte[] bytes) throws NoSuchAlgorithmException {
+        MessageDigest digest = MessageDigest.getInstance("SHA-256");
+        digest.update(bytes, 0, bytes.length);
+        byte[] hashedBytes = digest.digest();
+        StringBuilder hexString = new StringBuilder();
+        for (byte b : hashedBytes) {
+            hexString.append(String.format("%02x", b));
+        }
+        return hexString.toString();
+    }
+}
